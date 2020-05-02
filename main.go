326e3110c2a1be2678d28d4c92fed175b0923f07
sdjyliqi <sdjyliqi@163.com>
2020-05-02 22:35:33 +0800

    基本框架

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/chenjiandongx/ginprom"
 	"github.com/gin-gonic/gin"
 	"github.com/sdjyliqi/feirars/middleware"
 	"github.com/sdjyliqi/feirars/router"
@@ -10,7 +9,6 @@ import (
 func main() {
 	r := gin.Default()
 	r.Use(middleware.RequestIDMiddleware())
-	r.Use(ginprom.PromMiddleware(nil))
 	// register the `/metrics` route.
 	router.InitRouter(r)
 	r.Run(":8899")
