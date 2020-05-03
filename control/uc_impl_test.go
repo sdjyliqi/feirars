29a802091fd646552d4b578dfa3546fa79b0cfff
sdjyliqi <sdjyliqi@163.com>
2020-05-03 13:22:24 +0800

    屏蔽多余代码

--- control/uc_impl_test.go
+++ control/uc_impl_test.go
@@ -1,18 +1,14 @@
 package control
 
-//相关封装函数的UT
-import (
-	"testing"
-)
-
-func Test_Login(t *testing.T) {
-
-}
-
-func Test_Register(t *testing.T) {
-
-}
-
-func Test_Logout(t *testing.T) {
-
-}
+//
+//func Test_Login(t *testing.T) {
+//
+//}
+//
+//func Test_Register(t *testing.T) {
+//
+//}
+//
+//func Test_Logout(t *testing.T) {
+//
+//}
