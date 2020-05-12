5343b91477b74a1b05b5a0b569809493a261106b
leelele <sdjyliqi@163.com>
2020-05-12 23:42:04 +0800

    Update config.go

--- conf/config.go
+++ conf/config.go
@@ -9,7 +9,7 @@ type FeirarConfig struct {
 
 // DefaultConfig .
 var DefaultConfig = FeirarConfig{
-	DBMysql: "pingback-0001a:Pinback-123987!@tcp(123.57.37.83:3306)/pingback?charset=utf8mb4",
+	DBMysql: "pingback-0001a:passport@tcp(127.0.0.1:3306)/pingback?charset=utf8mb4",
 	DBRedis: "redis://1:@127.0.0.1:6379",
 	IPLOC:   ".\\data\\GeoLite2-City.mmdb",
 }
