0beb720fa4a14a3043a37abff66caa3376d1a3aa
leeqi <sdjyliqi@163.com>
2021-01-31 20:53:15 +0800

    chart channels

---
+++ models/sjtb_full_test.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"encoding/json"
+	"github.com/sdjyliqi/feirars/testutil"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func Test_GetChartItems(t *testing.T) {
+	testMod := SjtbFull{}
+	items, err := testMod.GetChartItems(testutil.TestMysql, "all", time.Now().Unix()-3600*24*7, time.Now().Unix())
+	assert.Nil(t, err)
+	t.Log(items, err)
+	content, _ := json.Marshal(items)
+	t.Log(string(content))
+}
